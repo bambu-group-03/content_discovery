b4899557725ba6c4832594e034654b41177ee572
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-10-26 00:05:22 -0300

    fix(app): CORS

--- content_discovery/web/application.py
+++ content_discovery/web/application.py
@@ -2,6 +2,7 @@ from importlib import metadata
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import UJSONResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -13,6 +14,16 @@ from content_discovery.web.lifetime import (
 )
 
 APP_ROOT = Path(__file__).parent.parent
+origins = [
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    # "http://localhost",
+    # "http://localhost:3000", # backoffice localhost
+    # "http://next-app:3000", # backoffice docker microservice network
+    # "http://localhost:9000", # content
+    # NOT WORK IN DOCKER 🥺😥
+    "*",
+]
 
 
 def get_app() -> FastAPI:
@@ -31,6 +42,13 @@ def get_app() -> FastAPI:
         redoc_url=None,
         openapi_url="/api/openapi.json",
         default_response_class=UJSONResponse,
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     # Adds startup and shutdown events.
