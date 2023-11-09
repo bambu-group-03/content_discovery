7a7fc6a63ccea6ea3f415a3086c4b884588a170b
Nox <davela@fi.uba.ar>
2023-11-09 00:51:41 -0300

    hotfix

--- content_discovery/db/migrations/versions/2023-11-01-22-44_844c236e9e58.py
+++ content_discovery/db/migrations/versions/2023-11-01-22-44_844c236e9e58.py
@@ -5,6 +5,8 @@ Revises: 5dc2a44b23db
 Create Date: 2023-11-01 22:44:28.102272
 
 """
+import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "844c236e9e58"
@@ -14,8 +16,20 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        "hashtags",
+        sa.Column("id", sa.Uuid(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("snap_id", sa.Uuid()),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["snap_id"],
+            ["snaps.id"],
+            name=op.f("fk_hashtag_snap_id"),
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("hashtags")
--- content_discovery/db/migrations/versions/2023-11-02-01-17_def43c5d88af.py
+++ content_discovery/db/migrations/versions/2023-11-02-01-17_def43c5d88af.py
@@ -6,6 +6,9 @@ Create Date: 2023-11-02 01:17:43.907540
 
 """
 
+import sqlalchemy as sa
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision = "def43c5d88af"
 down_revision = "844c236e9e58"
@@ -13,9 +16,21 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade() -> None:
-    pass
+op.create_table(
+    "mentions",
+    sa.Column("id", sa.Uuid(), nullable=False),
+    sa.Column("mentioned_id", sa.String(), nullable=True),
+    sa.Column("mentioned_username", sa.String(), nullable=True),
+    sa.Column("snap_id", sa.Uuid()),
+    sa.Column("created_at", sa.DateTime(), nullable=False),
+    sa.ForeignKeyConstraint(
+        ["snap_id"],
+        ["snaps.id"],
+        name=op.f("fk_mention_snap_id"),
+    ),
+    sa.PrimaryKeyConstraint("id"),
+)
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("mentions")
