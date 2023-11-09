ac6a39f80de9a2a6c5e92ac0faae50513c03802c
Nox <davela@fi.uba.ar>
2023-11-09 00:41:31 -0300

    hotfix

--- content_discovery/db/migrations/versions/2023-11-02-01-17_def43c5d88af.py
+++ content_discovery/db/migrations/versions/2023-11-02-01-17_def43c5d88af.py
@@ -5,8 +5,6 @@ Revises: 844c236e9e58
 Create Date: 2023-11-02 01:17:43.907540
 
 """
-import sqlalchemy as sa
-from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "def43c5d88af"
@@ -16,21 +14,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        "mentions",
-        sa.Column("id", sa.Uuid(), nullable=False),
-        sa.Column("mentioned_id", sa.String(), nullable=True),
-        sa.Column("mentioned_username", sa.String(), nullable=True),
-        sa.Column("snap_id", sa.Uuid()),
-        sa.Column("created_at", sa.DateTime(), nullable=False),
-        sa.ForeignKeyConstraint(
-            ["snap_id"],
-            ["snaps.id"],
-            name=op.f("fk_mention_snap_id"),
-        ),
-        sa.PrimaryKeyConstraint("id"),
-    )
+    pass
 
 
 def downgrade() -> None:
-    op.drop_table("mentions")
+    pass
