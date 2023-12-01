8b0d1133072e4784f40705c53e1f40e3cfc2d584
Nox <davela@fi.uba.ar>
2023-12-01 03:28:43 -0300

    Remove unused code

--- content_discovery/web/api/router.py
+++ content_discovery/web/api/router.py
@@ -1,6 +1,6 @@
 from fastapi.routing import APIRouter
 
-from content_discovery.web.api import docs, dummy, echo, feed, filter, interactions
+from content_discovery.web.api import docs, feed, filter, interactions
 
 api_router = APIRouter()
 api_router.include_router(feed.router, prefix="/feed", tags=["feed"])
@@ -10,6 +10,4 @@ api_router.include_router(
     tags=["interactions"],
 )
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(filter.router, prefix="/filter", tags=["filter"])
