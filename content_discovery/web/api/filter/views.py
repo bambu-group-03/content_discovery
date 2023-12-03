b2ca35bff37952354b73e963e1e27a64476fad64
Nox <davela@fi.uba.ar>
2023-12-02 21:37:17 -0300

    Add admin filter snaps endpoint

--- content_discovery/web/api/filter/views.py
+++ content_discovery/web/api/filter/views.py
@@ -1,10 +1,16 @@
+from typing import Any
+
 from fastapi import APIRouter
 from fastapi.param_functions import Depends
 
 from content_discovery.db.dao.hashtag_dao import HashtagDAO
 from content_discovery.db.dao.snaps_dao import SnapDAO
 from content_discovery.web.api.feed.schema import FeedPack
-from content_discovery.web.api.utils import complete_snaps, followed_users
+from content_discovery.web.api.utils import (
+    complete_snaps,
+    followed_users,
+    get_user_info,
+)
 
 router = APIRouter()
 
@@ -32,3 +38,36 @@ async def filter_snaps(
     users = followed_users(user_id)
     snaps = await snaps_dao.filter_snaps(content, users)
     return await complete_snaps(snaps, user_id, snaps_dao)
+
+
+@router.get("/admin/content", response_model=None)
+async def admin_filter_snaps(
+    content: str,
+    snaps_dao: SnapDAO = Depends(),
+) -> Any:
+    """Retrieve a list of filtered snaps by content."""
+    completed_snaps = []
+
+    snaps = await snaps_dao.admin_filter_snaps(content)
+
+    for snap in snaps:
+        (username, fullname, _url) = get_user_info(snap.user_id)
+
+        completed_snaps.append(
+            {
+                "id": snap.id,
+                "user_id": snap.user_id,
+                "content": snap.content,
+                "likes": snap.likes,
+                "shares": snap.shares,
+                "favs": snap.favs,
+                "created_at": snap.created_at,
+                "username": username,
+                "fullname": fullname,
+                "parent_id": snap.parent_id,
+                "visibility": snap.visibility,
+                "privacy": snap.privacy,
+            },
+        )
+
+    return completed_snaps
